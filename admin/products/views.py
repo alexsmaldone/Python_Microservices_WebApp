61cbd7fb840ebc7b2a908f1b3c28323a200597f9
alexsmaldone <alexsmaldone@gmail.com>
2022-06-08 22:58:34 -0400

    product viewset

--- admin/products/views.py
+++ admin/products/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+class ProductViewSet(viewsets.ViewSet):
+  def list(self, request): #/api/products
+    pass
+
+  def create(self, request): #/api/products
+    pass
+
+  def retrieve(self, request, pk=None):
+    pass
+
+  def update(self, request, pk=None):
+    pass
+
+  def delete(self, request, pk=None):
+    pass
