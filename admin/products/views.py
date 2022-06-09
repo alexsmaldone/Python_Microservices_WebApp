e8f938047997f3c3f93d6b2f0992eb32276cbc1c
alexsmaldone <alexsmaldone@gmail.com>
2022-06-09 17:32:37 -0400

    product viewstatus create

--- admin/products/views.py
+++ admin/products/views.py
@@ -1,4 +1,4 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, status
 from rest_framework.response import Response
 
 from .models import Product
@@ -10,10 +10,11 @@ class ProductViewSet(viewsets.ViewSet):
     serializer = ProductSerializer(products, many=True)
     return Response(serializer.data)
 
-
-
   def create(self, request):
-    pass
+    serializer = ProductSerializer(data=request.data)
+    serializer.is_valid(raise_exception=True)
+    serializer.save()
+    return Response(serializer.data, status=status.HTTP_201_created)
 
   def retrieve(self, request, pk=None):
     pass
